0ebc19c68d1a1076d394cdc2700fec09cbcf5154
Matteo Abis <gmatteo..abis@gmail.com>
2018-02-27 10:32:54 +0100

    typo

--- telegrambot/bot.py
+++ telegrambot/bot.py
@@ -34,7 +34,7 @@ def main(v):
     updater.dispatcher.add_handler(CommandHandler('dovesiamo', cc.dovesiamo))
     updater.dispatcher.add_handler(CommandHandler('oflgu210', cc.oflgu210))
     updater.dispatcher.add_handler(CommandHandler('mozart', cc.mozart))
-    updater.dispatcher.add_handler(CommandHandler('15240100103043', cc.opera_solved))
+    updater.dispatcher.add_handler(CommandHandler('15640100103043', cc.opera_solved))
     updater.dispatcher.add_handler(
         MessageHandler(
             ~telegram.ext.filters.Filters.location,

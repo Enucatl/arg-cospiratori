0e8b357e0c68905f83abadeb49443e6d4b4d601f
Matteo Abis <gmatteo..abis@gmail.com>
2018-02-27 10:21:00 +0100

    deu

--- telegrambot/bot.py
+++ telegrambot/bot.py
@@ -30,7 +30,7 @@ def main(v):
     updater.dispatcher.add_handler(CommandHandler('crocedoriente', cc.croce_oriente))
     updater.dispatcher.add_handler(CommandHandler('abulafia', cc.abulafia))
     updater.dispatcher.add_handler(CommandHandler('borges', cc.borges))
-    updater.dispatcher.add_handler(CommandHandler('germania', cc.mozartlaugh))
+    updater.dispatcher.add_handler(CommandHandler('deutschland', cc.mozartlaugh))
     updater.dispatcher.add_handler(CommandHandler('dovesono', cc.dovesono))
     updater.dispatcher.add_handler(CommandHandler('dovesiamo', cc.dovesiamo))
     updater.dispatcher.add_handler(CommandHandler('oflgu210', cc.oflgu210))
